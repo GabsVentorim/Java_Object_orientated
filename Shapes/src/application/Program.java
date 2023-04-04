6aeffb4bce10d22d1654bdf58e7f8b3b08021562
Gabriel Ventorim Silva <106477470+GabsVentorim@users.noreply.github.com>
2023-04-04 12:57:00 -0300

    add application

---
+++ Shapes/src/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+import entities.AbstractShape;
+import entities.Circle;
+import entities.Color;
+import entities.Rectangle;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		AbstractShape c = new Circle(Color.WHITE, 2.0); 
+		AbstractShape r = new Rectangle(Color.WHITE, 2.0, 3.0); 
+		
+	}
+
+}
